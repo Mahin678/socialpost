d0a7f18af72e640c02369e02a7602c9ac86a9ea1
Mahin <mahintazuar678@gmail.com>
2020-09-06 04:57:23 +0600

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+import Banner from './Components/Banner/Banner';
+import Post from './Components/Post/Post';
+import Categorys from './Components/Categorys/Categorys';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Comments from './Components/Comments/Comments';
+import Allpost from './Components/AllPost/Allpost';
+import PrimarySearchAppBar from './Components/PrimarySearchAppBar/PrimarySearchAppBar';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header></Header>
+        <Switch>
+          <Route path="/Home">
+            <Banner></Banner>
+            <div className="wrapper d-flex " >
+              <div className="postComponent w-75 mt-4">
+                <Post></Post>
+              </div>
+              <div className="categoryComponent" >
+                <Categorys></Categorys>
+              </div>
+            </div>
+          </Route>
+          <Route path="/Post">
+            <Banner></Banner>
+            <Allpost></Allpost>
+          </Route>
+          <Route path="/comment/:id">
+            <Comments></Comments>
+          </Route>
+          <Route path="/About Project">
+            <h1>Site is comming</h1>
+          </Route>
+          <Route exact path="/">
+            <div className="wrapper d-flex " >
+              <div className="postComponent w-75  ">
+                <Post></Post>
+              </div>
+              <div className="categoryComponent" >
+                <Categorys></Categorys>
+              </div>
+            </div>
+          </Route>
+          <Route path="*" >
+            <h1> Site is not found (404)</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { Button } from '@material-ui/core';
+import './Banners.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexWrap: "wrap"
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: "25ch"
+    }
+  }));
+const Banner = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+                  <div className="web-Banner" >
+                <h1>Have a Question?</h1>
+                <p>If you have any question you can ask below or enter what you are looking for!</p>
+                <div className="input-group input-group-lg w-50 mx-auto">
+                       <div className={classes.root}>
+        <TextField
+          id="outlined-full-width"
+          style={{ margin: 8,width:"30rem",backgroundColor:"white" }}
+          placeholder="Placeholder"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true
+          }}
+          variant="outlined"
+        />
+      </div>
+                    <div className="input-group-prepend">
+                    <Button variant="contained" color="primary"  >
+  Search
+</Button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
